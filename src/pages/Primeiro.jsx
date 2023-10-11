782ce3118e8b22bd5816a8008d047195d91825b8
EduardoMangeli <eduardo@mangeli.com.br>
2023-10-11 16:47:16 -0300

    configurando bibliotecas

--- src/pages/Primeiro.jsx
+++ src/pages/Primeiro.jsx
@@ -1,7 +1,10 @@
+import LinkSegundo from "../components/LinkSegundo";
+
 const Primeiro = () => {
  return (<div>
             <h5>mini título</h5>
             <p>Meu texto</p>
+            <LinkSegundo />
         </div>);
 };
 
