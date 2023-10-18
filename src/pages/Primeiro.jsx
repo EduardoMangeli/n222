13f02966f1cf6726dd14d8c775492aec152a6032
EduardoMangeli <eduardo@mangeli.com.br>
2023-10-18 16:39:46 -0300

    link generico

--- src/pages/Primeiro.jsx
+++ src/pages/Primeiro.jsx
@@ -1,10 +1,28 @@
+import LinkGenerico from "../components/LinkGenerico";
 import LinkSegundo from "../components/LinkSegundo";
 
+
 const Primeiro = () => {
+    const dados = [
+        ["https://www.ibmec.br/", "ibmec"],
+
+        ["https://estude.ibmec.br/ibmec-hubs", "ibmec-hubs"],
+
+        ["https://botafogo.com.br/", "botafogo"]
+    ];
+
+    const links = dados.map( (ele, i) => (
+        <LinkGenerico
+            key={i} 
+            texto={ele[1]}
+            endereco={ele[0]}
+        />
+    ))
+
  return (<div>
             <h5>mini título</h5>
             <p>Meu texto</p>
-            <LinkSegundo />
+            {links}
         </div>);
 };
 
