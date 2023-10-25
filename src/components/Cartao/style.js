e85e54cadd02e1898f92096bd45098acc4af94b4
EduardoMangeli <eduardo@mangeli.com.br>
2023-10-25 16:24:53 -0300

    elemento cartao

---
+++ src/components/Cartao/style.js
@@ -0,0 +1,17 @@
+import { styled } from "styled-components";
+
+const ContainerCartao = styled.div`
+    display: flex;
+    flex-direction: column;
+
+    p{
+        text-align: center;
+        font-weight: bolder;
+    }
+`;
+
+const Titulo = styled.h3`
+    color: forestgreen;
+`
+
+export {ContainerCartao, Titulo}
