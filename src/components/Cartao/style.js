5eb2015db38ad071f8e5a43c60f9d7e76117cfa4
EduardoMangeli <eduardo@mangeli.com.br>
2023-10-25 16:56:30 -0300

    criando listagem

--- src/components/Cartao/style.js
+++ src/components/Cartao/style.js
@@ -3,6 +3,7 @@ import { styled } from "styled-components";
 const ContainerCartao = styled.div`
     display: flex;
     flex-direction: column;
+    border: solid black 1px;
 
     p{
         text-align: center;
